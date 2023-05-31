3f9dd71ab8049cc471fb7f5a112bf378ed388788
Matias Grimm <matiasgrimm@gmail.com>
2023-05-31 14:38:37 +0200

    Create User.cs

---
+++ MyGarden/Models/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyGarden.Models.Entities
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = null!;
+    }
+}
